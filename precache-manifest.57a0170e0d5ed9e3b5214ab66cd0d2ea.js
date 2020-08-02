bd6c324b3b3f54fa47fd791d3714c75197621390
sverba <verba.dp@gmail.com>
2020-08-02 12:19:42 +0300

    Updates

---
+++ precache-manifest.57a0170e0d5ed9e3b5214ab66cd0d2ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4458e84ca82de7b3f779e88437ecab7",
+    "url": "/yal/index.html"
+  },
+  {
+    "revision": "e9d674634dd83de181db",
+    "url": "/yal/static/css/main.5af2bcf7.chunk.css"
+  },
+  {
+    "revision": "df179ec8a5a6643e6ea9",
+    "url": "/yal/static/js/2.ad899240.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yal/static/js/2.ad899240.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9d674634dd83de181db",
+    "url": "/yal/static/js/main.536e425b.chunk.js"
+  },
+  {
+    "revision": "9f9559c82a1b84fa581a",
+    "url": "/yal/static/js/runtime-main.16fa8688.js"
+  }
+]);
